ddcb9661696c1b6293fbc5599d0963b81e5e8c97
harinesh-balamurugan <harineshslm@gmail.com>
2024-07-17 22:02:19 +0200

    The following code change is to complete the babynames exercise

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -41,7 +41,52 @@ def extract_names(filename):
   ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
   """
   # +++your code here+++
-  return
+
+  # Opening the file
+
+  f = open(filename, encoding='utf-8')
+
+  # Importing re module to execute the findall and search method of the library
+
+  import re
+
+  # Reading the contents of the file into a string varaible file_read_string
+
+  file_read_string = f.read()
+
+  # Finding all the matches for the rank,   boyname and girlname
+
+  matching_line_of_rank_boyname_girlname = re.findall(r'<tr.+><td>(\d+).+>(\w+)<.+>(\w+)', file_read_string)
+
+  # Intiatiling the array for boyname, girlname and year of the file
+
+  boy_name = []
+  girl_name = []
+  year_of_file=[]
+
+  # Extracting the year from the file
+
+  full_heading_text = re.search(r'(<h3.+>)', file_read_string)
+  year_on_heading = re.search(r'\s\d+',full_heading_text.group())
+
+  # Appending the year on the file to the year array
+
+  year_of_file.append(year_on_heading.group().strip())
+
+  # Extracting the boyname with rank and girlname with rank and append it to the boyname and girlname array
+
+  for i in matching_line_of_rank_boyname_girlname:
+    boy_name.append(i[1]+" "+ i[0])
+    girl_name.append(i[2]+" "+ i[0])
+  
+  # Combine both the girlname and boyname into a combined array named as 'Total_name'
+
+  total_name = boy_name + girl_name
+
+  # Concat the year array with the sorted Total_name array
+
+  print(year_of_file+sorted(total_name))
+  return year_of_file+sorted(total_name)
 
 
 def main():
@@ -62,7 +107,17 @@ def main():
 
   # +++your code here+++
   # For each filename, get the names, then either print the text output
-  # or write it to a summary file
+  # or write it to a summary 
+  # file
+  for filename in args:
+   output_of_the_sorted_names = extract_names(filename)
+
+   if summary:
+     with open(filename + '.summary', "w", encoding='utf-8') as f:
+      for i in output_of_the_sorted_names:
+       
+       f.write(i + '\n')
+      f.close()
 
 if __name__ == '__main__':
   main()
